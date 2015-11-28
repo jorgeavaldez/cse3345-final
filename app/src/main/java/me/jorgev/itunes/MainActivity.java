1ecd62b981e503d0ba7e723b6ad98e6817fa96b3
Jorge Valdez <valdez.a.jorge@gmail.com>
2015-11-27 21:03:37 -0600

    beginning of advanced settings

--- app/src/main/java/me/jorgev/itunes/MainActivity.java
+++ app/src/main/java/me/jorgev/itunes/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 import com.bumptech.glide.Glide;
 
@@ -13,11 +17,17 @@ import hugo.weaving.DebugLog;
 
 public class MainActivity extends AppCompatActivity {
 
+    public boolean advancedViewShowing;
+    public LinearLayout advancedViewRoot;
+    View advancedSettingsView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        advancedViewShowing = false;
+        advancedViewRoot = (LinearLayout) findViewById(R.id.main_layout);
+        advancedSettingsView = getLayoutInflater().inflate(R.layout.advanced_search, null);
     }
 
 //    @DebugLog
@@ -34,5 +44,37 @@ public class MainActivity extends AppCompatActivity {
     @DebugLog
     public void onSearchButton(View target) {
 
+    }
+
+    // This function is the callback for when the advanced search options button is called.
+    // It should inflate the 'advanced_search' layout under the buttons and all that jazz.
+    @DebugLog
+    public void onAdvancedButton(View target) {
+        if (!this.advancedViewShowing) {
+            advancedViewRoot.addView(advancedSettingsView);
+            initSpinner();
+
+            this.advancedViewShowing = true;
+        }
+
+        else {
+            advancedViewRoot.removeView(advancedSettingsView);
+            this.advancedViewShowing = false;
+        }
+    }
+
+    @DebugLog
+    public void initSpinner() {
+        String[] spinnerItems = new String[] {"Artist", "Track", "Album", "Video", "Mix", "Song"};
+
+        Spinner s = (Spinner) findViewById(R.id.result_type);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, spinnerItems);
+        s.setAdapter(adapter);
+
+        String imageURL = "http://i.giphy.com/yShdlgJ3ZVdVm.gif";
+
+        ImageView imageView = (ImageView) findViewById(R.id.cheeseburger_img);
+
+        Glide.with(this).load(imageURL).into(imageView);
     }
 }
